e4a28a1a20080441f295e9580dac78956b459b4e
Shoukang <80080583+ShoukangHong@users.noreply.github.com>
2021-04-16 21:29:34 -0400

    Update SRPG_AuraSkill.js

--- SRPG_AuraSkill.js
+++ SRPG_AuraSkill.js
@@ -1,7 +1,7 @@
 //=============================================================================
 // SRPG_AuraSkill.js
 //-----------------------------------------------------------------------------
-// Free to use and edit   v1.04 Now you can show Aura range on movetable!
+// Free to use and edit   v1.05 Now Unitevent can have Aura effect!
 //=============================================================================
 /*:
  * @plugindesc This plugin allows you to create Aura skills for SRPG battle. Place it below all SRPG plugins for best compatibility.
@@ -42,46 +42,44 @@
  * This plugin provides several note tags for you to create Aura skills. 
  * An aura skill will add a state automatically to valid units in Aura range.
  * Passive Aura skills can be created by skill notetags. It will assign the state to valid units within the Aura range.
- *
- * skill note tags:
- * <SRPGAuraState:x>    this is the state of this Aura skill, replace x with state id.
- * <SRPGAuraTarget:xxx> This is the units that will be affected, xxx can be "friend" "foe" or "all" (no need to add "")
+ * Active aura skills can be created by state notetags. You can actively use a skill to gain an "Aura state", as long as this Aura state
+ * exist it will assign a (different) state to the valid units within the Aura range. (Credits to Boomy)
+ * This also allows you to activate aura effects in other ways(add this aura state by script calls, or whatever else)
+ * Now Events with <type:unitEvent> can also have passive Aura.
+ * =========================================================================================================================
+ * skill/event/state note tags:
+ * <SRPGAuraState:x>    this is the state this skill/event/state will assign (to valid units), replace x with state id.
+ * <SRPGAuraTarget:xxx> This is the units that will be affected, xxx can be "friend" "foe" or "all". For unitevent, friend is actor and foe is enemy.(no need to add quote)
  * <SRPGAuraRange:x>    The range of Aura, similar to AoE range.
  * <SRPGAuraShape:xxx>  The shape of Aura, replace xxx with shapes defined in SRPR_AoE (Anisotropic shapes not supported)
  * <SRPGAuraMinRange:x> The minumum range of Aura, creats a hole. Default is 0.
- * <SRPGAuraColor:xxx>  The color of this Aura skill.
- * <SRPGShowAura:x>     If have this notetag, show this aura on movetable.
+ * <SRPGAuraColor:xxx>  The color of this Aura skill. https://www.w3schools.com/cssref/css_colors.asp
+ * <SRPGShowAura:x>     If have this notetag, show this aura's range on movetable.
  * You may also want to use state note tag <SRPGAura> (see below).
  * 
- * Active aura skill can be created by state notetags. You can actively use a skill to gain an "Aura state", as long as this Aura state
- * exist it will assign a (different) state to the valid units within the Aura range. (Credits to Boomy)
- * This also allows you to activate aura effects in other ways(add this aura state by script calls, or whatever else)
- *
  * state note tag:
  * <SRPGAura>           With this notetag a state will be removed once a unit is out of the Aura.
  * If you want the Aura to be effective after a unit leaves the Aura range don't use this tag.
  *
- * <SRPGAuraState:x>    This is the state this "Aura state" will assign, replace x with state id.
- * <SRPGAuraTarget:xxx> These are the same as skill notw tags.
- * <SRPGAuraRange:x>    
- * <SRPGAuraShape:xxx>  
- * <SRPGAuraMinRange:x>
- * <SRPGAuraColor:xxx>
- * <SRPGShowAura:x>
+ * event note tag:
+ * <SRPGAuraPage:x>     On which event page is the aura active. If don't write the aura is always active (unless it's erased).
  *
  * Aura states of related units will be refreshed everytime you open the SRPGstatuswindow, 
  * prediction window, menu window. It will also refresh when show movetable, before battle, after action, battle start and turn end.
  * You can also assign Aura skills to enemies.
  * You may want to use some other plugins like ALOE_ItemSkillSortPriority to put a passive aura skill to the end of 
  * your skill list.
- * 
- * version 1.00 first release!
- * version 1.01 refresh status when open main menu. fix some bugs.
- * version 1.02 refresh status when turn end.
- * version 1.03 add state note tags for active aura skills. Fix issues of states without <SRPGAura>
+ * ==========================================================================================================================
+ * version 1.05 support aura for unitevents!
  * version 1.04 show aura range on movetable!
- *
+ * version 1.03 add state note tags for active aura skills. Fix issues of states without <SRPGAura>
+ * version 1.02 refresh status when turn end.
+ * version 1.01 refresh status when open main menu. fix some bugs.
+ * version 1.00 first release!
+ * ===========================================================================================================================
+ * Compatibility:
  * This plugin needs SPPG_AoE to work. Place this plugin below SRPG_ShowAoERange, SRPG_BattleUI if you are using them.
+ * ===========================================================================================================================
  */
 (function () {
 	var parameters = PluginManager.parameters('SRPG_AuraSkill');
@@ -175,6 +173,7 @@
 			if (statelist[i].meta.SRPGAura) {
 //                this.removeState(statelist[i].id);
 				this._states.splice(i, 1);
+				this.refresh();//v1.05 added, not sure if it's necessary
 			}
 		}
 	};
@@ -196,6 +195,10 @@
 				unit[1].states().forEach(function(item){//check all states
 					if ($gameTemp.isAuraStateValid(item, userevent.isType(), unit[0], dx, dy)) user.addState(Number(item.meta.SRPGAuraState));
 				});
+			} else if (event.isType() === 'unitEvent'){
+				var item = event.event()
+				if (item.meta.SRPGAuraPage && Number(item.meta.SRPGAuraPage) != event.pageIndex()) return;
+				if ($gameTemp.isAuraStateValid(item, userevent.isType(), 'actor', dx, dy)) user.addState(Number(item.meta.SRPGAuraState));
 			}
 		});
 	};
@@ -274,9 +277,9 @@
 
 	var shoukang_Sprite_SrpgMoveTile_initialize = Sprite_SrpgMoveTile.prototype.initialize;
 	Sprite_SrpgMoveTile.prototype.initialize = function() {
-		shoukang_Sprite_SrpgMoveTile_initialize.call(this);
-		this.aura = false;
-	};
+        shoukang_Sprite_SrpgMoveTile_initialize.call(this);
+        this.aura = false;
+    };
 
 	var shoukang_Sprite_SrpgMoveTile_clearThisMoveTile = Sprite_SrpgMoveTile.prototype.clearThisMoveTile;
 	Sprite_SrpgMoveTile.prototype.clearThisMoveTile = function() {
@@ -284,12 +287,12 @@
 		this.aura = false;
 	}
 
-	Sprite_SrpgMoveTile.prototype.updateAnimation = function() {
+    Sprite_SrpgMoveTile.prototype.updateAnimation = function() {
 		this._frameCount++;
 		this._frameCount %= 90;
 		if (!this.aura) this.opacity = 210 - Math.abs(this._frameCount - 45) * 2;
 		else this.opacity = Math.abs(this._frameCount - 45) * 5 - 90;
-	};
+    };
 
 	if (!Game_Enemy.prototype.skills) {
 		Game_Enemy.prototype.skills = function() {
